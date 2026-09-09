ae081b76b65e821627081755dc9fddd2ed8c1e96
alfkar <93591000+alfkar@users.noreply.github.com>
2026-09-09 11:17:57 +0200

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        hi = max(piles) 
+        lo = 1
+        mid = (hi+lo)//2
+        bestRate = hi
+        while lo <= hi:
+            if self.canFinish(mid, piles, h):
+                hi = mid-1
+                bestRate = min(bestRate, mid)
+            else:
+                lo = mid+1
+            mid = (hi+lo)//2
+        return bestRate 
+    def canFinish(self, rate: int, piles: list[int], h: int) -> bool:
+        sumHours = 0
+        for pile in piles:
+            pile_time = -(pile//-rate)
+            sumHours += pile_time
+            if(sumHours > h):
+                return False
+        return True
